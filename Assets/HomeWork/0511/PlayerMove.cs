7c2139640658cdf72de5d12200612835e6d54177
dMinsz <dsminzks@gmail.com>
2023-05-11 21:22:21 +0900

    docs: Add Comments for description

--- Assets/HomeWork/0511/PlayerMove.cs
+++ Assets/HomeWork/0511/PlayerMove.cs
@@ -23,8 +23,8 @@ public class PlayerMove: MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rgd = GetComponent<Rigidbody>();
-        name = "Player";
+        rgd = GetComponent<Rigidbody>(); // set rigidbody
+        name = "Player"; // set name
     }
 
     // Update is called once per frame
@@ -35,26 +35,26 @@ public class PlayerMove: MonoBehaviour
             0,
             Input.GetAxis("Vertical")
         );
-        // 이동방향 * 속도 * 프레임단위 시간을 곱해서 카메라의 트랜스폼을 이동
+        // 이동방향 * 속도 * 프레임단위 시간을 곱해서 오브젝트 이동
         transform.Translate(dir * moveSpeed * Time.deltaTime);
 
 
-        float yRotateSize = Input.GetAxis("Mouse X") * rotateSpeed;
+        float yRotateSize = Input.GetAxis("Mouse X") * rotateSpeed;//마우스의 횡이동을 가져와서
         float yRotate = transform.eulerAngles.y + yRotateSize;
 
-        transform.eulerAngles = new Vector3(-Input.GetAxis("Mouse Y"), yRotate, 0);
+        transform.eulerAngles = new Vector3(-Input.GetAxis("Mouse Y"), yRotate, 0); // 회전 시켜준다.
 
-        if (Input.GetKeyDown(KeyCode.Space) && IsJump)
+        if (Input.GetKeyDown(KeyCode.Space) && IsJump) // 스페이스 키가 눌리고 점프를 할수있다면
         {
             Debug.Log(name + "이 점프상태");
-            rgd.AddForce(Vector3.up * jumpSpeed ,ForceMode.Impulse);
+            rgd.AddForce(Vector3.up * jumpSpeed ,ForceMode.Impulse); //점프 를 한다.
             IsJump = false;
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Ground")) // 땅에 닿으면 점프를 할 수 있게 구현
         {
             Debug.Log(name + "이 땅에닿았다.");
             IsJump = true;
